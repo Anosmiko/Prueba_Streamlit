68910c10094b4918ee38e76bcda89f595a76111f
NC <psi.nicolas.coddou@gmail.com>
2023-05-09 21:21:01 -0400

    First Commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(page_title="Dashboard de Ventas",
+                    page_icon= ":bar_chart:",
+                    layout="wide")
+
+df = pd.read_excel("supermarkt_sales.xlsx",
+                    engine="openpyxl",
+                    sheet_name="Sales",
+                    skiprows=3,
+                    usecols="B:R",
+                    nrows=1000)
+
+st.dataframe(df)
